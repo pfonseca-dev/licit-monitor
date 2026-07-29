764d25cbbfdc50f81298c9a9632a1b5c67984119
pfonseca-dev <pfmartins2006@gmail.com>
2026-07-29 14:37:33 -0300

    chore(setup): Configurar estrutura do FastAPI rodando no Docker, ORM e validar Swagger

---
+++ back_end/app/models/__init__.py
@@ -0,0 +1,9 @@
+from app.models.base import Base
+from app.models.licitacao import Licitacao
+from app.models.dispensas import Dispensas
+
+__all__ = [
+    "Base",
+    "Licitacao",
+    "Dispensas",
+]
